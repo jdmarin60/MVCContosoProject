4aed4fc3ec94bc645cfdbfb1a0286f4ec261d6ed
ing2gdoc <ing2gdoc@btw.com.co>
2023-04-20 08:35:28 -0500

    Modify Controller

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -19,6 +19,7 @@ namespace ContosoUniversity.Controllers
         // GET: Course
         public ActionResult Index()
         {
+            
             var courses = db.Courses.Include(c => c.Department);
             return View(courses.ToList());
         }
